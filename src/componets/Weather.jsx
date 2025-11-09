6f404eac6a913e5d82dbd1cce931f8bee60aa036
carlo <carlolalas4@gmail.com>
2025-11-09 16:50:20 +0800

    add response if city is not found

--- src/componets/Weather.jsx
+++ src/componets/Weather.jsx
@@ -48,6 +48,12 @@ const search = async (city) => {
     
     const response = await fetch(url);
     const data = await response.json();
+
+    if(!response.ok){
+      alert(data.message);
+      return;
+    }
+
     console.log(data);
     const icon = allIcons[data.weather[0].icon] || clear_icon;
     setWeatherData({
